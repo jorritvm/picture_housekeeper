3be5b85605ab219630bab1b0b1b998f466e48424
Jorrit Vander Mynsbrugge <44178217+jorritvm@users.noreply.github.com>
2024-11-13 16:44:13 +0100

    include prefixnames in readme

--- src-prefixnames/prefix_names_mtime.py
+++ src-prefixnames/prefix_names_mtime.py
@@ -1,43 +1,42 @@
 # prefix all filenames with a iso8601 mtime
 
-
 import os
 from datetime import datetime
 
-flat_folder = r"U:\family\pictures\2023\2023-07-16 Denemarken\denemarken-emmy"
+base_folder = r"U:\family\pictures\2024\_2024-05 Gevelwerken Mabrick uitvoering"
 
-
-def main():
+def main(base_folder):
     x = input("are you sure you wish to proceed? (y/n)")
-    if x != "y":
+    if x.lower() != "y":
         print("Aborting..")
-    else:
-        print("-----------------------")
-        print("Renaming all files with mtime prefix.")
-        # first do all required conversions
-        all_files = os.listdir(flat_folder)
+        return  # early exit!
+    
+    print("-----------------------")
+    print("Renaming all files with mtime prefix.")
+    # first do all required conversions
+    all_files = os.listdir(base_folder)
 
-        for file_name in all_files:
-            file_name_full = os.path.join(flat_folder, file_name)
-            base, ext1 = os.path.splitext(file_name)
-            root, ext = os.path.splitext(file_name_full)
+    for old_file_name in all_files:
+        old_file_path = os.path.join(base_folder, old_file_name)
+        _, ext = os.path.splitext(old_file_name)
 
-            if ext.lower() in [".jpg", ".mov", ".png"]:
-                mtime = os.path.getmtime(file_name_full)
+        if ext.lower() in [".jpg", ".mov", ".png"]:
+            mtime = os.path.getmtime(old_file_path)
 
-                # Convert the timestamp to a datetime object
-                modified_datetime = datetime.fromtimestamp(mtime)
+            # Convert the timestamp to a datetime object
+            modified_datetime = datetime.fromtimestamp(mtime)
 
-                # Format the datetime object as an ISO 8601 string
-                iso8601_string = modified_datetime.isoformat().replace(":", "-")
+            # Format the datetime object as an ISO 8601 string
+            iso8601_string = modified_datetime.isoformat().replace(":", "-")
 
-                # rename
-                new_filename = f"{iso8601_string}_{file_name}"
-                new_file_path = os.path.join(flat_folder, new_filename)
-                os.rename(file_name_full, new_file_path)
+            # rename
+            new_file_name = f"{iso8601_string}_{old_file_name}"
+            new_file_path = os.path.join(base_folder, new_file_name)
+            # print(f"OLD: {old_file_name}\nNEW: {new_file_name}\n--------------")
+            os.rename(old_file_path, new_file_path)
 
-        print("Finished!")
+    print("Finished!")
 
 
 if __name__ == "__main__":
-    main()
+    main(base_folder)
